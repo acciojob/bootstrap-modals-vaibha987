f77fd43d4576f28cea32c14fd3b4446eced7de00
vaibha987 <v2goyal95@gmail.com>
2024-07-02 18:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+//your JS code here. If required.
+var modal = document.querySelector('.modal');
+var openModalButton = document.getElementById('open-modal');
+var closeModalButton = document.querySelector('.close-modal');
+openModalButton.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+closeModalButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
